6722e669f766d946404eaf8de072829cf0b16a01
Vosters <mike.r.vosters@gmail.com>
2014-02-03 22:44:17 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chilly::Application.config.secret_key_base = '09eb5ce180830e4fc446da5369aab78cf967c0c880dabb3cfe47023310613b8a8071465e1004e37a8e7f4661c8a4e790e0428dcb951908467a1f7e4e4a646a7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chilly::Application.config.session_store :cookie_store, key: '_chilly_session'
